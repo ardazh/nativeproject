f3e3ef2cd552f3b8643715a7979e77b7d2127e9f
ardazh <rifaardan@gmail.com>
2023-08-20 22:23:40 +0700

    feat: create eventId & transaction reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,6 +5,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import deviceToken from './deviceToken';
 import profileData from './profileData';
 import eventData from './eventData';
+import eventId from './eventId';
+import transaction from './transaction';
 
 const authConfig = {
   key: 'auth',
@@ -16,6 +18,8 @@ const reducer = combineReducers({
   deviceToken,
   profileData,
   eventData,
+  eventId,
+  transaction,
 });
 
 export default reducer;
